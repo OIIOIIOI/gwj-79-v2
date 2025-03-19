485659dcccdacdd6eaa005f55dcbd3abfcd50c7b
OIIOIIOI <sylvere.armange@gmail.com>
2025-03-19 15:01:15 +0100

    weight lifting ok

--- scenes/mini_games/weight_lifting/weight_lifting.gd
+++ scenes/mini_games/weight_lifting/weight_lifting.gd
@@ -4,6 +4,8 @@ class_name WeightLifting
 
 const UP_SPEED := 80.0
 const DOWN_SPEED := 50.0
+const WIN_HOLD_THRESHOLD := 0.85
+const WIN_HOLD_DURATION := 3.0
 
 enum GAME_STATE {
 	Initializing,
@@ -14,14 +16,22 @@ enum GAME_STATE {
 
 var game_state: GAME_STATE = GAME_STATE.Initializing
 var target_position := Vector2.ZERO
+var held_time := 0.0
 
 
 @onready var arm: Node2D = $Arm
 @onready var start_marker: Marker2D = $StartMarker
 @onready var end_marker: Marker2D = $EndMarker
 
+@onready var mayor_arm: Node2D = $MayorArm
+@onready var mayor_start_marker: Marker2D = $MayorStartMarker
+@onready var mayor_end_marker: Marker2D = $MayorEndMarker
+@onready var mayor_win_marker: Marker2D = $MayorWinMarker
+
+
 
 func _ready() -> void:
+	sync_mayor_position(0.0)
 	animate_start()
 
 
@@ -52,7 +62,48 @@ func _process(delta: float) -> void:
 		target_position.y = clampf(target_position.y, end_marker.global_position.y, start_marker.global_position.y)
 		arm.global_position = target_position
 
+		# Update win progress
+		if get_progress() >= WIN_HOLD_THRESHOLD:
+			held_time += delta
+		else:
+			held_time = 0.0
+
+		# Sync mayor thumbs up
+		sync_mayor_position(get_hold_progress())
+
+		# If win, end game
+		if get_hold_progress() == 1.0:
+			end_game()
+
 
 func get_progress() -> float:
 	var r = inverse_lerp(start_marker.global_position.y, end_marker.global_position.y, arm.global_position.y)
 	return abs(clampf(r, 0.25, 1.0))
+
+
+func get_hold_progress() -> float:
+	var r = inverse_lerp(0.0, WIN_HOLD_DURATION, held_time)
+	return abs(clampf(r, 0.0, 1.0))
+
+
+func sync_mayor_position(progress: float) -> void:
+	mayor_arm.global_position = mayor_start_marker.global_position.lerp(mayor_end_marker.global_position, progress)
+	mayor_arm.rotation = lerp(mayor_start_marker.rotation, mayor_end_marker.rotation, progress)
+	mayor_arm.scale = mayor_start_marker.scale.lerp(mayor_end_marker.scale, progress)
+
+
+func end_game() -> void:
+	game_state = GAME_STATE.End
+
+	var tween := create_tween()
+	tween.set_parallel()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(mayor_arm, ^"global_position", mayor_win_marker.global_position, 0.5)
+	tween.tween_property(mayor_arm, ^"rotation", mayor_win_marker.rotation, 0.5)
+	tween.tween_property(mayor_arm, ^"scale", mayor_win_marker.scale, 0.5)
+
+	await tween.finished
+	await get_tree().create_timer(0.75).timeout
+
+	SceneTransition.transition_to(SceneTransition.SCENE_MAIN)
