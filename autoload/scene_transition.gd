f93d48ce5da2854e50d3b4533a08ef6aaaff05a7
OIIOIIOI <sylvere.armange@gmail.com>
2025-03-19 21:42:54 +0100

    ramp test, hammer time WIP

--- autoload/scene_transition.gd
+++ autoload/scene_transition.gd
@@ -8,12 +8,14 @@ const SCENE_TITLE := "scene_title"
 const SCENE_MAIN := "scene_main"
 const SCENE_MEMORY := "scene_memory"
 const SCENE_WEIGHT_LIFTING := "scene_weight_lifting"
+const SCENE_HAMMER_TIME := "scene_hammer_time"
 
 const scenes: Dictionary = {
 	SCENE_TITLE: preload("res://scenes/ui/title_screen/title_screen.tscn"),
 	SCENE_MAIN: preload("res://scenes/main/main.tscn"),
 	SCENE_MEMORY: preload("res://scenes/mini_games/memory/memory.tscn"),
 	SCENE_WEIGHT_LIFTING: preload("res://scenes/mini_games/weight_lifting/weight_lifting.tscn"),
+	SCENE_HAMMER_TIME: preload("res://scenes/mini_games/hammer_time/hammer_time.tscn"),
 }
 
 
